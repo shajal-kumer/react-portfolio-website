bc54f3f283a3f08b85e9aef31c11fcd89707c80b
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-09-27 16:31:16 +0600

    Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Personal Portfolio
 
+# This is the practice project from jsmastery youtube channel
+
 ![Portfolio Website](https://i.ibb.co/WgPMpts/image.png)
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => (
 		</Div1>
 		<Div2>
 			<li>
-				<Link href="#projets">
+				<Link href="#projects">
 					<NavLink>Projects</NavLink>
 				</Link>
 			</li>
